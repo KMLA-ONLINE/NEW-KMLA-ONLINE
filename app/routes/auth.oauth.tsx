0e84f05a8c9b76b6200c2dea990a96b4ebb0a7cd
Jeonguk Choi <dev@cjeong.uk>
2026-05-19 20:31:55 +0900

    Fix wrong import statements

--- app/routes/auth.oauth.tsx
+++ app/routes/auth.oauth.tsx
@@ -1,12 +1,12 @@
-import { redirect, type LoaderFunctionArgs } from 'react-router'
+import { redirect, type LoaderFunctionArgs } from "react-router"
 
-import { createClient } from '@/registry/default/clients/react-router/lib/supabase/server'
+import { createClient } from "~/lib/supabase/server"
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
-  const _next = requestUrl.searchParams.get('next')
-  const next = _next?.startsWith('/') ? _next : '/'
+  const code = requestUrl.searchParams.get("code")
+  const _next = requestUrl.searchParams.get("next")
+  const next = _next?.startsWith("/") ? _next : "/"
   if (code) {
     const { supabase, headers } = createClient(request)
 
